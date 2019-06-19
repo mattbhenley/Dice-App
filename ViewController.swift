7ffd51681bc6d59c005b12ddca4e217b442bb443
Matt Henley <mattbhenley@gmail.com>
2019-06-18 20:36:38 -0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice App
+//
+//  Created by Matt Henley on 6/18/19.
+//  Copyright © 2019 Matt Henley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
