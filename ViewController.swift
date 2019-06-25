05432068bff7b5af5b88d25d7bed824dfd0c15f8
Matt Henley <mattbhenley@gmail.com>
2019-06-24 20:32:58 -0500

    added randomnization

--- ViewController.swift
+++ ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -19,6 +23,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
         
     }
     
