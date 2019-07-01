4f5a7f73463574f3a8126d55a58aaa3d2bd4ac18
Matt Henley <mattbhenley@gmail.com>
2019-07-01 17:02:45 -0500

    added motion detection

--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
@@ -19,16 +21,30 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateDicImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        updateDicImages()
+    }
+    
+    // added motion detection
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDicImages()
+    }
+    
+    // function for random dice images
+    func updateDicImages() {
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
         print(randomDiceIndex1)
         
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
